1e182a05e0bbd2977b82e8758178f794a28407d1
Manan Shah <manan.shah@pagabogroup.com>
2025-11-12 13:17:31 +0000

    image fixes

--- src/blocks/MediaBlock/Component.tsx
+++ src/blocks/MediaBlock/Component.tsx
@@ -38,7 +38,7 @@ export const MediaBlock: React.FC<Props> = (props) => {
         <div className="relative h-[1185px] overflow-hidden">
           {/* Image */}
           <Media
-            imgClassName="w-full object-cover h-[1485px]"
+            imgClassName="w-full object-cover translate-y-[100px] h-[1185px]"
             resource={media}
             src={staticImage}
           />

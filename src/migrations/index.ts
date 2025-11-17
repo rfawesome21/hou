2d8b2a0345b6c208e308e7bfe466c8dd622a8503
Manan Shah <manan.shah@pagabogroup.com>
2025-11-17 12:42:54 +0000

    remove migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,7 +1,6 @@
 import * as migration_20251112_114957 from './20251112_114957';
 import * as migration_20251112_115040 from './20251112_115040';
 import * as migration_20251117_122650 from './20251117_122650';
-import * as migration_20251117_123027 from './20251117_123027';
 
 export const migrations = [
   {

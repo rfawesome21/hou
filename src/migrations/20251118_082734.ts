ef0d5937910c063766b3f9803249b2b3972ea073
Manan Shah <manan.shah@pagabogroup.com>
2025-11-18 08:33:16 +0000

    update migration

--- src/migrations/20251118_082734.ts
+++ src/migrations/20251118_082734.ts
@@ -2,14 +2,27 @@ import { MigrateUpArgs, MigrateDownArgs, sql } from '@payloadcms/db-postgres'
 
 export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   await db.execute(sql`
-   CREATE TYPE "public"."enum_pages_blocks_about_section_left_width" AS ENUM('w-1/4', 'w-[30%]', 'w-1/3', 'w-[40%]', 'w-[45%]', 'w-1/2');
+  CREATE TYPE "public"."enum_pages_blocks_about_section_left_width" AS ENUM('w-1/4', 'w-[30%]', 'w-1/3', 'w-[40%]', 'w-[45%]', 'w-1/2');
   CREATE TYPE "public"."enum__pages_v_blocks_about_section_left_width" AS ENUM('w-1/4', 'w-[30%]', 'w-1/3', 'w-[40%]', 'w-[45%]', 'w-1/2');
-  ALTER TABLE "posts_rels" ALTER COLUMN "sizes_id" SET DATA TYPE integer;
-  ALTER TABLE "_posts_v_rels" ALTER COLUMN "sizes_id" SET DATA TYPE integer;
-  ALTER TABLE "sizes" ALTER COLUMN "id" SET DATA TYPE serial;
-  ALTER TABLE "payload_locked_documents_rels" ALTER COLUMN "sizes_id" SET DATA TYPE integer;
-  ALTER TABLE "pages_blocks_about_section" ADD COLUMN "left_width" "enum_pages_blocks_about_section_left_width" DEFAULT 'w-1/4';
-  ALTER TABLE "_pages_v_blocks_about_section" ADD COLUMN "left_width" "enum__pages_v_blocks_about_section_left_width" DEFAULT 'w-1/4';`)
+
+  ALTER TABLE "posts_rels"
+    ALTER COLUMN "sizes_id" TYPE integer USING "sizes_id"::integer;
+
+  ALTER TABLE "_posts_v_rels"
+    ALTER COLUMN "sizes_id" TYPE integer USING "sizes_id"::integer;
+
+  ALTER TABLE "sizes"
+    ALTER COLUMN "id" TYPE integer USING "id"::integer;
+
+  ALTER TABLE "payload_locked_documents_rels"
+    ALTER COLUMN "sizes_id" TYPE integer USING "sizes_id"::integer;
+
+  ALTER TABLE "pages_blocks_about_section"
+    ADD COLUMN "left_width" "enum_pages_blocks_about_section_left_width" DEFAULT 'w-1/4';
+
+  ALTER TABLE "_pages_v_blocks_about_section"
+    ADD COLUMN "left_width" "enum__pages_v_blocks_about_section_left_width" DEFAULT 'w-1/4';
+`)
 }
 
 export async function down({ db, payload, req }: MigrateDownArgs): Promise<void> {
